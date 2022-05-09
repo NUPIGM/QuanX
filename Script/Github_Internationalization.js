f77822dc65e00794bbc28cb72a54375623dcbb31
NUPIGM <53999409+NUPIGM@users.noreply.github.com>
2022-05-10 00:28:52 +0800

    Update Github_Internationalization.js

--- Script/Github_Internationalization.js
+++ Script/Github_Internationalization.js
@@ -1,5 +1,5 @@
 let ele = '<head>';
-let eleReplace = '<head><script type="text/javascript" async="async" src="https://raw.githubusercontent.com/NUPIGM/QuanX/main/Script/Github_Internationalization.user.js"></script>'
+let eleReplace = '<head><script type="text/javascript" async="async" src="https://raw.githubusercontent.com/NUPIGM/QuanX/main/Script/Github Internationalization.user.js"></script>'
 let body = $response.body
     .replace(ele, eleReplace)
 $done({ body });
