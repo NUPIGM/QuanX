2ecc724bea49e3ac999a4285c9f297cebc8a4f26
NUPIGM <53999409+NUPIGM@users.noreply.github.com>
2022-05-10 00:23:34 +0800

    Update Github_Internationalization.js

--- Script/Github_Internationalization.js
+++ Script/Github_Internationalization.js
@@ -1,5 +1,5 @@
 let ele = '<head>';
-let eleReplace = '<head><link rel="stylesheet" href="https://limbopro.com/CSS/Adblock4limbo.user.css" type="text/css" /><script type="text/javascript" async="async" src="https://limbopro.com/Adguard/Adblock4limbo.user.js"></script>'
+let eleReplace = '<head><script type="text/javascript" async="async" src="https://raw.githubusercontent.com/NUPIGM/QuanX/main/Script/Github_Internationalization.user.js"></script>'
 let body = $response.body
     .replace(ele, eleReplace)
 $done({ body });
