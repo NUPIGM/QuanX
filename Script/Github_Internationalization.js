3a22506040dca551a765167f228752eb304ed0f0
NUPIGM <53999409+NUPIGM@users.noreply.github.com>
2022-07-09 06:49:25 +0800

    Update and rename Script/Github_Internationalization.js to Scripts/Github_Internationalization.js

--- Script/Github_Internationalization.js
+++
@@ -1,5 +0,0 @@
-let ele = '<head>';
-let eleReplace = '<head><script type="text/javascript" async="async" src="https://github.com/NUPIGM/QuanX/raw/main/Script/GitHub_Internationalization.user.js"></script>'
-let body = $response.body
-    .replace(ele, eleReplace)
-$done({ body });
