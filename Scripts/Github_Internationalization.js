6266ac5d1dea8d0c667e5b69bb1e7aecc6afc008
NUPIGM <53999409+NUPIGM@users.noreply.github.com>
2022-07-09 10:38:57 +0800

    5
    5

--- Scripts/Github_Internationalization.js
+++
@@ -1,5 +0,0 @@
-let ele = '<head>';
-let eleReplace = '<head><script type="text/javascript" async="async" src="https://raw.githubusercontent.com/k1995/github-i18n-plugin/master/userscript.js"></script>'
-let body = $response.body
-    .replace(ele, eleReplace)
-$done({ body });
