529c18a66a54e8713acf3678eab561837813385a
NUPIGM <53999409+NUPIGM@users.noreply.github.com>
2022-07-09 08:39:30 +0800

    3
    3

--- Scripts/netflix.mom.js
+++ Scripts/netflix.mom.js
@@ -1,5 +1,5 @@
 let ele = '<head>';
-let eleReplace = '<head><script type="text/javascript" async="async" src="https://raw.githubusercontent.com/k1995/github-i18n-plugin/master/userscript.js"></script>'
+let eleReplace = '<head><link rel="stylesheet" href="https://raw.githubusercontent.com/NUPIGM/QuanX/main/Scripts/css/netflix.mom.css" type="text/css" />'
 let body = $response.body
     .replace(ele, eleReplace)
 $done({ body });
