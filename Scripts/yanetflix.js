bcbf9d93161ce03279e7adde3a26417ebbd5bfa5
NUPIGM <53999409+NUPIGM@users.noreply.github.com>
2022-07-15 10:30:47 +0800

    updata

--- Scripts/yanetflix.js
+++
@@ -1,5 +0,0 @@
-let ele = '<head>';
-let eleReplace = '<head><link rel="stylesheet" href="https://raw.githubusercontent.com/NUPIGM/QuanX/main/Scripts/css/netflix.mom.css" type="text/css" />'
-let body = $response.body
-    .replace(ele, eleReplace)
-$done({ body });
--- Scripts/zqzdzj.js
+++
@@ -1,1 +0,0 @@
-document.querySelector('.ec-ad').hidden = 'true';
