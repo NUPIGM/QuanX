3acad51d49177f7771c0382c5f98afd08f5dc532
NUPIGM <53999409+NUPIGM@users.noreply.github.com>
2023-06-05 03:08:43 +0800

    更新 Proxy.js

--- Surge/Mock/Proxy.js
+++ Surge/Mock/Proxy.js
@@ -1,4 +1,14 @@
-fetch('https://jsonplaceholder.typicode.com/posts/1')
+//解析URL属性值：?sub=<url>
+function queryURLParams() {
+      let url = window.location.search;
+      const urlSearchParams = new URLSearchParams(url);
+      const params = Object.fromEntries(urlSearchParams.entries());
+      return params;
+    }
+//提取订阅的网址
+let sub = queryURLParams();
+//获取订阅网址的内容
+fetch(sub)
   .then(response => response.json())
   .then(data => {
     // 在这里对数据进行处理或传递到其他函数
@@ -7,28 +17,16 @@ fetch('https://jsonplaceholder.typicode.com/posts/1')
   .catch(error => {
     console.error(error);
   });
-
+//将数据进行base64解析
 function processData(data) {
-  // 这里可以对数据进行进一步处理或使用
-  console.log(data);
-  // 也可以将数据传递到其他函数进行进一步处理
-  anotherFunction(data);
-}
-
-function anotherFunction(data) {
-  // 在这里可以对数据进行额外的处理或使用
-  console.log(data);
-}
-
-function queryURLParams() {
-      let url = window.location.search;
-      const urlSearchParams = new URLSearchParams(url);
-      const params = Object.fromEntries(urlSearchParams.entries());
-      return params;
+  const base64Config = data.split('vmess://');
+  for(let config in base64Config){
+    const decodedConfig = atob(config);
+  const jsonConfig = JSON.parse(decodedConfig);
     }
+}
 
-    let a = JSON.stringify(queryURLParams());
 
     // 将脚本放在末尾，确保DOM已加载
     document.getElementById('output').innerHTML = a;
-    console.log(a);
+
