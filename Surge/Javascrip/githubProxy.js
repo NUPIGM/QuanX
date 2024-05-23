062934ec1a7f18ab802d644b739a259b3bd7d09a
NUPIGM <53999409+NUPIGM@users.noreply.github.com>
2024-05-23 16:27:14 +0000

    up

--- Surge/Javascrip/githubProxy.js
+++ Surge/Javascrip/githubProxy.js
@@ -5,12 +5,12 @@
 let githubPrefix = "https://raw.githubusercontent.com/"
 let fastrawPrefix = "https://fastraw.ixnic.net/" //由FastGit群组成员 @duya1234567 提供，代号A镜像。
 let hubinceptPrefix = "https://hub.incept.pw/" // 由FastGit群组成员 @mxe365 提供，代号B镜像。
-let jsdelivrPrefix ="https://cdn.jsdelivr.net/gh/"
-let gitmirrorPrefix ="https://raw.gitmirror.com"
+let gitmirrorPrefix = "https://raw.gitmirror.com"
+// let jsdelivrPrefix = "https://cdn.jsdelivr.net/gh/"
 // let kkgithubPrefix = "https://raw.kkgithub.com/" //由KGithub提供，暂时失效。
 
 
-let changeTo = $argument
+const cdn = $argument
 
 var url = $request.url
 var headers = $request.headers
@@ -22,15 +22,23 @@ if (!url.startsWith(githubPrefix)) {
     return;
 }
 
-if (changeTo == "") {
-    headers["host"] = "A镜像"
-    url = url.replace(githubPrefix,fastrawPrefix)
-} else if (changeTo == "B镜像") {
-    headers["host"] = "hub.incept.pw"
-    url = url.replace(githubPrefix,hubinceptPrefix)
-} else if (changeTo == "C镜像") {
-    headers["host"] = "raw.kkgithub.com"
-    url = url.replace(githubPrefix,kkgithubPrefix)
+switch (cdn) {
+    case 1:
+        headers["host"] = "fastraw.ixnic.net"
+        url = url.replace(githubPrefix, fastrawPrefix)
+        break;
+    case 2:
+        headers["host"] = "hub.incept.pw"
+        url = url.replace(githubPrefix, hubinceptPrefix)
+        break;
+    case 3:
+        headers["host"] = "raw.gitmirror.com"
+        url = url.replace(githubPrefix, gitmirrorPrefix)
+        break;
+    default:
+        headers["host"] = "fastraw.ixnic.net"
+        url = url.replace(githubPrefix, fastrawPrefix)
+        break;
 }
 
-$done({url:url,headers:headers})
+$done({ url: url, headers: headers })
